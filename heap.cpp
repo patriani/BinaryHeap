9d30c7dbd23d4ffaf702e3a37c3ed8508049f716
Ricardo Bortolucci Patriani de Carvalho <bortolucci1@gmail.com>
2022-01-13 01:45:45 -0300

    Can check array order
    User can enter with number of inputs and the inputs in sequence to deploy an array.  If the elements in the current array doesnt obey the restriction of MaxHeap it'll be fixed.

--- heap.cpp
+++ heap.cpp
@@ -28,25 +28,24 @@ Treap;*/
 
 using namespace std;
 
-void ordena(int* vet, int n){ // void para int*
+int* corrige(int* vet, int n){ // void para int*
     bool in_order = true;
     int i = 1; //indice do vetor começando em 1;
 
     while(true){
-        
-        printf("\ndentro\n");
-        
+                
         if( (2*i+1) <= n ){ // se o índice consta no vetor então será analisado;
-            if( vet[i-1] < vet[(2*i)] ){ //condição MaxHeap;
-                int temp1 = vet[i-1];
+                                        // vet[i-1]: PAI , vet[(2*i)]: filho direita.
+            if( vet[i-1] < vet[(2*i)] ){// condição MaxHeap.
+                int temp1 = vet[i-1];   // invertendo pai com filho.
                 int temp2 = vet[2*i];
                 vet[i-1] = temp2;
                 vet[2*i] = temp1;
                 in_order = false;
             }
         }
-        if( (2*i) <= n ){ // se o índice consta no vetor então será analisado;
-            if( vet[i-1] < vet[(2*i-1)] ){ //condição MaxHeap;
+        if( (2*i) <= n ){
+            if( vet[i-1] < vet[(2*i-1)] ){ 
                 int temp1 = vet[i-1];
                 int temp2 = vet[2*i-1];
                 vet[i-1] = temp2;
@@ -56,26 +55,22 @@ void ordena(int* vet, int n){ // void para int*
         }
         if((2*i) == n || (2*i+1) == n ){
             if(in_order){
-                break;
+                return(vet); // para a execução e retorna vetor verificado
             }else{
-                printf("\n\n\nhello");
                 in_order = true;
                 i = 1;
             }
         }
+        i++;
     }
-    int cont = 0;
-    while(cont < n){
-        printf("cont == %d vet[cont] == %d ",cont,vet[cont]);
-        cont++;
-    }
-    printf("\n");
 }
 
 int main(){
     int* vet = NULL;
     int n = 0; // tamanho vetor
     int cont = 0;
+    
+    int* recebe_vet = NULL;
 
     printf("\ninsira tamanho entrada:\n");
     scanf("%d",&n);
@@ -88,8 +83,14 @@ int main(){
         vet[cont] = temp;
         cont++;
     }
-    printf("antes");
-    ordena(vet,n);
+
+    recebe_vet = corrige(vet,n);
+    cont = 0;
+
+    while(cont < n){
+        printf("vet[%d] = %d\n",cont,recebe_vet[cont]);
+        cont++;
+    }
 
     return 0;
 
